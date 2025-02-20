1bdf4f6c0e615f49af834fc69d3956dc2ca0a242
Jannatul <limajannat1675@gmail.com>
2025-02-21 02:12:39 +0600

    added problemSolving2

--- problem1.js
+++ problem1.js
@@ -1,4 +1,4 @@
-// Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+//problem-2 Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
 function nextEdge(side1, side2) {
 	let maxTriangle = (side1 + side2) - 1;
 	return maxTriangle;
---
+++ problem2.js
@@ -0,0 +1,6 @@
+// # Power Calculator
+//problem-2 Create a function that takes voltage and current and returns the calculated power.
+function circuitPower(voltage, current) {
+	let power = voltage * current;
+	return power;
+}
