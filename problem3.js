7042b82283f73364c1209752e674a052854a2e98
Jannatul <limajannat1675@gmail.com>
2025-02-21 02:25:11 +0600

    added problemSolving3

---
+++ problem3.js
@@ -0,0 +1,6 @@
+// # Convert Hours into Seconds
+// problem-3 Write a function that converts hours into seconds.
+function howManySeconds(hours) {
+	let hourToSeconds = hours * 60 * 60;
+	return hourToSeconds;
+}
