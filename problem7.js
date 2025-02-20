570cd857878a387f06b7e23ab4d094898e0f3f5f
Jannatul <limajannat1675@gmail.com>
2025-02-21 02:55:00 +0600

    added problemSolving7

---
+++ problem7.js
@@ -0,0 +1,6 @@
+// # Return Something to Me!
+// Problem-7: Write a function that returns the string "something" joined with a space " " and the given argument a.
+function giveMeSomething(a) {
+	let connect ="something" + " " + a;
+	return connect;
+}
