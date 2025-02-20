d25baecd454a9946e6f5d4de53512b1e4ecfa6c5
Jannatul <limajannat1675@gmail.com>
2025-02-21 02:30:43 +0600

    added problemSolving4

---
+++ problem4.js
@@ -0,0 +1,6 @@
+// # Convert Minutes into Seconds
+// Problem-4 Write a function that takes an integer minutes and converts it to seconds.
+function convert(minutes) {
+	let minutesToSeconds = minutes * 60;
+    return minutesToSeconds;
+}
