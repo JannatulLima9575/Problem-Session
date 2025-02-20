9061d1a9c1e2c9738fc32ddf65c8dca4cec4a363
Jannatul <limajannat1675@gmail.com>
2025-02-21 03:25:59 +0600

    added problemSolvig11

---
+++ problem11.js
@@ -0,0 +1,7 @@
+// # Basic Variable Assignment
+// Problem: A student learning JavaScript was trying to make a function. His code should concatenate a passed string name with string "Edabit" and store it in a variable called result. He needs your help to fix this code.
+function nameString(name){
+	let b = "Edabit";
+	var result = name + b;
+  	return result;
+}
