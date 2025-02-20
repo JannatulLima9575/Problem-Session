6a0750dda4bc030b5674fcc6e1e72085e49f0746
Jannatul <limajannat1675@gmail.com>
2025-02-21 04:44:24 +0600

    added problemSolving12

---
+++ problem12.js
@@ -0,0 +1,6 @@
+// # Sum of Polygon Angles
+// Problem- 12: Given an n-sided regular polygon n, return the total sum of internal angles (in degrees).
+function sumPolygon(n) {
+	let polygon =(n - 2) * 180 ;
+    return polygon; 
+}
