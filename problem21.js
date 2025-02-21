c3777c959c55beb4e918dfb5ae5ad8bf201e3157
Jannatul <limajannat1675@gmail.com>
2025-02-21 16:41:44 +0600

    added problemSolving21

--- problem21.js
+++ problem21.js
@@ -1,1 +1,10 @@
-// Problem-21:
+// # Fix the Expression
+// Problem-21: Fix the code in the Code tab so the function returns true if and only if x is equal to 7.
+function isSeven(x) {
+	if( x === 7) {
+        return true;
+    }
+    else {
+        return false
+    }
+}
