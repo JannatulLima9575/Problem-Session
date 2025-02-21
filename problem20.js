d9e5e6f1b1d59c31f8a5d5f1a09b98cf5061a9dc
Jannatul <limajannat1675@gmail.com>
2025-02-21 16:36:58 +0600

    added problemSolving20

--- problem20.js
+++ problem20.js
@@ -1,1 +1,5 @@
-// Problem-20:
+// # Convert Hours and Minutes into Seconds
+// Problem-20: Write a function that takes two integers (hours, minutes), converts them to seconds, and adds them.
+function convert(hours, minutes) {
+	return (hours * 60 * 60) + (minutes * 60);
+}
