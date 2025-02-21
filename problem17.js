3d206eb1a8a3da334887e9367f3acbae8c980b5f
Jannatul <limajannat1675@gmail.com>
2025-02-21 16:09:24 +0600

    added problemSolving17

--- problem17.js
+++ problem17.js
@@ -1,1 +1,10 @@
-// Problem-17:
+// # Using the "&&" Operator
+// Problem-17: JavaScript has a logical operator &&. The && operator takes two boolean values, and returns true if both values are true.
+function checkLogic( a, b) {
+    if ( a && b) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
