87fab7f5128dfb9d0f5d147b841c6f4a2cb9f98e
Jannatul <limajannat1675@gmail.com>
2025-02-21 16:30:53 +0600

    added problemSolving19

--- problem19.js
+++ problem19.js
@@ -1,1 +1,5 @@
-// Problem-19:
+// # Football Points
+// Problem-19: Create a function that takes the number of wins, draws and losses and calculates the number of points a football team has obtained so far.
+function footballPoints(wins, draws, losses) {
+	return wins *3 + draws * 1 + losses * 0;
+}
