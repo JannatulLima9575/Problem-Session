26f763ffbd112162096d82014db420c3f947fc94
Jannatul <limajannat1675@gmail.com>
2025-02-23 19:36:55 +0600

    added problemSolving22

--- problem22.js
+++ problem22.js
@@ -1,1 +1,5 @@
-// Problem-22:
+// # Equality Check
+// Problem-22: In this challenge, you must verify the equality of two different values given the parameters a and b.
+function checkEquality(a, b) {
+    return a === b;
+}
